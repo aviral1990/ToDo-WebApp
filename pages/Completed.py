389e5c75bb66241cc2ae04b400c5bd63fcbc30ed
Aviral-PC <aviral17@hotmail.com>
2023-01-29 18:28:41 +0530

    Enhancements to Completed List

--- pages/Completed.py
+++ pages/Completed.py
@@ -5,5 +5,16 @@ from Modules import functions
 
 completed_todo_list=functions.read_txt_file('completed.txt')
 
-for item in completed_todo_list:
-    st.write(item)
+st.title("Completed ToDos List")
+if(len(completed_todo_list)==0):
+    st.write("Completed ToDos is Empty!!")
+else:
+    for item in completed_todo_list:
+        st.write("--> "+item)
+
+    button=st.button("Delete all")
+    if(button):     #If Button Pressed
+        completed_todo_list.clear()
+        functions.write_txt(completed_todo_list,'completed.txt')
+        st.experimental_rerun()  # rerun the webpage script
+
